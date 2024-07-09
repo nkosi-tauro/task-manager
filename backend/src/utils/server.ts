b531cbd073c45fc6ee56cece9a9f2d8c331d450b
nkosi-tauro <nkosilati23@yahoo.com>
2024-07-09 12:04:25 +0200

    dummy health endpoint

--- backend/src/utils/server.ts
+++ backend/src/utils/server.ts
@@ -1,8 +1,14 @@
 import fastify from "fastify";
+import sequelize from "../database/connection";
 
 export async function createServer() {
   const server = fastify({
     logger: true
+  });
+
+  //dummy Health check endpoint (update)
+  server.get('/health', async (request, reply) => {
+    return { status: 'ok' };
   });
 
   return server;
