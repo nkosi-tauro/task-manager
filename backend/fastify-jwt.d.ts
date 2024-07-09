b16fba61c3c2001e9b796f19c1097e458accd682
nkosi-tauro <nkosilati23@yahoo.com>
2024-07-09 18:30:36 +0200

    declaration file for fastify.authenticate

---
+++ backend/fastify-jwt.d.ts
@@ -0,0 +1,10 @@
+// fastify-jwt.d.ts
+import 'fastify';
+import fastify from 'fastify';
+
+declare module 'fastify' {
+  interface FastifyInstance extends
+  FastifyJwtNamespace<{namespace: 'security'}>{
+    authenticate: (request: fastify.FastifyRequest, reply: fastify.FastifyReply) => Promise<void>;
+  }
+}
