bf993dfb82eadbe7e209a20e39e90fdb015cca35
nkosi-tauro <nkosilati23@yahoo.com>
2024-07-09 18:31:09 +0200

    added jwt plugins for access control

--- backend/src/utils/server.ts
+++ backend/src/utils/server.ts
@@ -1,19 +1,32 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { logger } from "./logger";
 import userRoutes from "../modules/user/user.route";
 import fastifyPrintRoutes from "fastify-print-routes";
+import fastifyJwt from "@fastify/jwt";
 
 export async function createServer() {
   const server = fastify({
-    logger,
-    
+    logger,  
   });
   // For Testing
   await server.register(fastifyPrintRoutes)
 
-  server.register(userRoutes, { prefix: "/api/users" });
+  //JWT
+  server.register(fastifyJwt, {
+    secret: process.env.JWT_SECRET || 'verysecretsecret',
+  });
+  server.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (err) {
+      reply.send(err);
+    }
+  });
 
-  //Health check endpoint (update)
+  //User routes
+  server.register(userRoutes, { prefix: "/api/users" });
+  
+  //Dummy Health check endpoint 
   server.get('/health', async () => {
     return { status: 'ok' };
   });
