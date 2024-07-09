f5eaff26d8fab17a5c3de740fbc282393077c730
nkosi-tauro <nkosilati23@yahoo.com>
2024-07-09 16:35:33 +0200

    updated service name

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,9 +1,9 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { CreateUser } from "./user.services";
+import { createUser } from "./user.services";
 
 export async function createUserHandler(request: FastifyRequest, reply: FastifyReply) {
   try {
-    const user = await CreateUser(request.body as {email: string, password: string});
+    const user = await createUser(request.body as {email: string, password: string});
     return reply.code(201).send(user);
   } catch (error) {
     return reply.code(500).send({ error: "An error occurred while creating the user." });
